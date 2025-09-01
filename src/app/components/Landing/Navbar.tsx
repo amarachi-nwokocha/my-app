1781df08e0087f57c53ceef888ca50102b98303f
amarachi-nwokocha <nwokichaamarachi@gmail.com>
2025-09-01 22:00:51 +0100

    added the url

--- src/app/components/Landing/Navbar.tsx
+++ src/app/components/Landing/Navbar.tsx
@@ -10,7 +10,7 @@ const navItems = [
   { label: 'Staff', href: '#staff' },
   { label: 'Gallery', href: '#gallery' },
   { label: 'Contact', href: '#contact' },
-   { label: '7for7', href: '/' },
+   { label: '7for7', href: 'https://voting.pharoahshoundtattoostudios.com/' },
 ];
 
 const Navbar = () => {
