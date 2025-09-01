c669525c1001863db20d15151f09cad00f112147
amarachi-nwokocha <nwokichaamarachi@gmail.com>
2025-09-01 05:09:19 +0100

    one more try

--- src/app/components/Landing/Navbar.tsx
+++ src/app/components/Landing/Navbar.tsx
@@ -10,7 +10,7 @@ const navItems = [
   { label: 'Staff', href: '#staff' },
   { label: 'Gallery', href: '#gallery' },
   { label: 'Contact', href: '#contact' },
-   { label: '7for7', href: 'https://7for7.vercel.app/' },
+   { label: '7for7', href: '/7for7' },
 ];
 
 const Navbar = () => {
